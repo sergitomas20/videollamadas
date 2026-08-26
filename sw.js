9b4ca119e4612a26647c5b918612e07dd4a4ba11
sergitomas20 <sergi.tomas20@gmail.com>
2026-08-26 15:07:08 +0200

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "luma-v1";
+const CORE = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(req)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy));
+        return response;
+      })
+      .catch(() => caches.match(req).then(hit => hit || caches.match("./index.html")))
+  );
+});
